c0282d1473d074ae98cf9bbe7ef64b9b504b0755
reaganthomas <reagan_thomas@baylor.edu>
2015-09-07 20:44:22 -0500

    bump deps && bump task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 (function() {
   var gulp = require('gulp');
   var mocha = require('gulp-mocha');
+  var bump = require('gulp-bump');
 
   gulp.task('test', function() {
     return gulp.src('test/**/*.js', { read: false })
       .pipe(mocha());
+  });
+
+  gulp.task('bump', function() {
+    return gulp.src('./package.json')
+      .pipe(bump())
+      .pipe(gulp.dest('.'));
   });
 })();
